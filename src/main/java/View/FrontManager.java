7eedb660f4143030ab8cd55b5c9bc27296021685
Calle <Calle@Calle-Dator>
2014-02-04 13:29:04 +0100

    Hikari is bitch

---
+++ src/main/java/View/FrontManager.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package View;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Calle
+ */
+@ManagedBean
+@SessionScoped
+public class FrontManager {
+    
+    private String name;
+    private String lastName;    
+    private String email;
+    
+    private String transactionFailure;
+    
+    /**
+     * Creates a new instance of LoginManager
+     */
+    public FrontManager() {
+    }
+    
+    public void login () {
+        transactionFailure = "a";
+    }
+
+   /**
+     * @return <code>true</code> if the latest transaction succeeded, otherwise
+     * <code>false</code>.
+     */
+    public boolean getSuccess() {
+        return transactionFailure == null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String newLast) {
+        this.lastName = newLast;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String newEmail) {
+        this.email = newEmail;
+    }
+
+    public String getTransactionFailure() {
+        return transactionFailure;
+    }
+
+    public void setTransactionFailure(String transactionFailure) {
+        this.transactionFailure = transactionFailure;
+    }
+    
+    
+}
