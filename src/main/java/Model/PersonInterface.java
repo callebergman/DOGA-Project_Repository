a7684878331ee526c9a5bb2c5452af83287e1d45
Fredrik <Fredrik@Fredrik-Dator>
2014-02-06 17:41:15 +0100

    Merge origin/master
    Conflicts:
    	src/main/java/Model/CompetenceDTO.java

---
+++ src/main/java/Model/PersonInterface.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+;
+
+/**
+ *
+ * @author Fredrik
+ */
+public interface PersonInterface 
+{
+    public String getName();
+    public String getSurname();
+    public int getSsn();
+    public String getEmail();
+    public String getPassword();
+    public String getUsername();
+}
